13e88d449cc9ce57fb23138db03c5c972feac28a
Kshitij-cse <1719kshitij@gmail.com>
2024-12-15 11:21:55 +0530

    trash_deleted

--- test.py
+++
@@ -1,7 +0,0 @@
-from utils import *
-import pandas as pd
-
-df = pd.read_excel('read.xlsx')
-password = "tdye giwa ounx wuvp"
-recipients = ["1719kshitij@gmail.com"]
-send_email_with_dataframe(recipients,df,password)
